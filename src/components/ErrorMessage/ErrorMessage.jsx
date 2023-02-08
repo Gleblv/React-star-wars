f6516f54c29edaca1379a45bf6f9eca84359bcda
Gleb Leus <lv.gleb44@gmail.com>
2023-02-08 14:03:37 +0300

    goc-helper with error

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,15 @@
+import './ErrorMessage.css';
+
+const ErrorMessage = () => {
+    return (
+        <>
+        <div className="error__text">
+            The dark side of the forse has won. <br />
+            We can't display data. <br />
+            Come back when we fix everything.
+        </div>
+        </>
+    )
+}
+
+export default ErrorMessage;
--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import PeopleList from '../../components/PeoplePage/PeopleList/PeopleList';
+import withErrorApi from '../../hoc-helpers/withErrorApi';
 
 import { API_PEOPLE } from '../../constants/api';
 import { getApiResourse } from '../../utils/network';
@@ -8,25 +9,30 @@ import { getPeopleId, getPeopleImage } from '../../services/getPeopleData';
 
 import './PeoplePage.css';
 
-const PeoplePage = () => {
+const PeoplePage = ({setErrorApi}) => {
     const [peopl, setPeople] = useState(null);
 
     const getResourse = async (url) => {
         const res = await getApiResourse(url);
 
-        const peopleList = res.results.map(({name, url}) => {
-            const id = getPeopleId(url);
-            const img = getPeopleImage(id);
-
-            return {
-                id,
-                name,
-                url,
-                img
-            }
-        });
-
-        setPeople(peopleList);
+        if (res) {
+            const peopleList = res.results.map(({name, url}) => {
+                const id = getPeopleId(url);
+                const img = getPeopleImage(id);
+    
+                return {
+                    id,
+                    name,
+                    url,
+                    img
+                }
+            });
+    
+            setPeople(peopleList);
+            setErrorApi(false);
+        } else {
+            setErrorApi(true);
+        }
     }
 
     useEffect(() => {
@@ -40,4 +46,4 @@ const PeoplePage = () => {
     )
 }
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage);
